f6c388f26bb1a1d636d2eaa808ab43a33524a0e0
Stephen Kaczmarowski <stephenkaczmarowski@Stephens-MacBook-Pro.local>
2023-02-09 22:06:14 -0700

    add post section of the feed component

--- components/Feed.js
+++ components/Feed.js
@@ -1,7 +1,28 @@
 import { SparklesIcon } from '@heroicons/react/outline';
 import Input from './Input';
+import Post from './Post';
 
 export default function Feed() {
+    const posts = [
+        {
+            id: '1',
+            name: 'Stephen Kaczmarowski',
+            username: 'Kazman1596',
+            userImg: 'http://localhost:3000/_next/image?url=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F664169149002874880%2Fz1fmxo00_400x400.jpg&w=256&q=75',
+            img: 'https://i0.wp.com/theelectrichawk.com/wp-content/uploads/2023/01/Gem-Jam-2023-Lineup.jpg?resize=529%2C662&ssl=1',
+            text: 'Great lineup this year!',
+            timestamp: '2 hours ago'
+        },{
+            id: '2',
+            name: 'Stephen Kaczmarowski',
+            username: 'Kazman1596',
+            userImg: 'http://localhost:3000/_next/image?url=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F664169149002874880%2Fz1fmxo00_400x400.jpg&w=256&q=75',
+            img: 'https://theelectrichawk.com/wp-content/uploads/2022/01/Gem-and-Jam-2022-EH.jpg',
+            text: 'Had a blast!',
+            timestamp: '5 months ago'
+        }
+    ]
+
     return (
         <div className='xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl'>
             <div className='flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200'>
@@ -11,6 +32,9 @@ export default function Feed() {
                 </div>
             </div>
             <Input />
+            {posts.map((post)=> (
+                <Post key={post.id} post={post} />
+            ))}
         </div>
     );
 }
