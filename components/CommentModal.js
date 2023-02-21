ee191a9a1c9a74d34fdfb1c2bd1ee2530f50de40
Stephen Kaczmarowski <stephenkaczmarowski@Stephens-MacBook-Pro.local>
2023-02-21 11:48:36 -0700

    create post page

--- components/CommentModal.js
+++ components/CommentModal.js
@@ -34,7 +34,7 @@ const CommentModal = () => {
 
         setOpen(false)
         setInput('')
-        router.push(`posts/${postId}`)
+        router.push(`/posts/${postId}`)
     }
 
     return (
--- components/Post.js
+++ components/Post.js
@@ -10,24 +10,26 @@ import { deleteObject, ref } from "firebase/storage";
 import { useRecoilState } from "recoil";
 import { modalState } from "../atom/modalAtom";
 import { postIdState } from "../atom/postIdAtom";
+import { useRouter } from "next/router";
 
-const Post = ({post}) => {
+const Post = ({post, id}) => {
     const {data: session} = useSession()
     const [ likes, setLikes ] = useState([])
     const [ comments, setComments ] = useState([])
     const [ hasLiked, setHasLiked ] = useState(false)
     const [open, setOpen] = useRecoilState(modalState)
     const [postId, setPostId] = useRecoilState(postIdState)
+    const router = useRouter()
 
     useEffect(() => {
         const unsubscribe = onSnapshot(
-            collection(db, 'posts', post.id, 'likes'), (snapshot) => setLikes(snapshot.docs)
+            collection(db, 'posts', id, 'likes'), (snapshot) => setLikes(snapshot.docs)
         )
     }, [db])
     
     useEffect(() => {
         const unsubscribe = onSnapshot(
-            collection(db, 'posts', post.id, 'comments'), (snapshot) => setComments(snapshot.docs)
+            collection(db, 'posts', id, 'comments'), (snapshot) => setComments(snapshot.docs)
         )
     }, [db])
 
@@ -38,10 +40,10 @@ const Post = ({post}) => {
     async function likePost() {
         if(session) {
             if(hasLiked) {
-            await deleteDoc(doc(db, 'posts', post.id, 'likes', session?.user.uid))
+            await deleteDoc(doc(db, 'posts', id, 'likes', session?.user.uid))
 
         }else {
-            await setDoc(doc(db, 'posts', post.id, 'likes', session?.user.uid ), {
+            await setDoc(doc(db, 'posts', id, 'likes', session?.user.uid ), {
                 username: session.user.username,
             })
         }} else {
@@ -51,10 +53,11 @@ const Post = ({post}) => {
         
     async function deletePost() {
         if (window.confirm("Are you sure you want to delete this post?")) {
-            deleteDoc(doc(db, 'posts', post.id))
+            deleteDoc(doc(db, 'posts', id))
             if (post.data().image) {
-                deleteObject(ref(storage, `posts/${post.id}/image`))
+                deleteObject(ref(storage, `posts/${id}/image`))
             }
+            router.push('/')
         }
 
     }
@@ -62,7 +65,7 @@ const Post = ({post}) => {
     return (
         <div className="flex p-3 cursor-pointer border-b border-gray-200">
             {/* user image */}
-            <Image className="h-11 w-11 rounded-full mr-4" src={post.data().userImg} width='50' height='50' alt='user_image' />
+            <Image className="h-11 w-11 rounded-full mr-4" src={post?.data()?.userImg} width='50' height='50' alt='user_image' />
             {/* right side */}
             <div>
                 {/* Header */}
@@ -70,10 +73,10 @@ const Post = ({post}) => {
                 <div className="flex items-center justify-between">
                     {/* Post User Info */}
                     <div className="flex items-center space-x-1 whitespace-nowrap">
-                        <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">{post.data().name}</h4>
-                        <span className="text-sm sm:text-[15px]">@{post.data().username} - </span>
+                        <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">{post?.data()?.name}</h4>
+                        <span className="text-sm sm:text-[15px]">@{post?.data()?.username} - </span>
                         <span className="text-sm sm:text-[15px] hover:underline">
-                            <Moment fromNow>{post?.data().timestamp?.toDate()}</Moment>
+                            <Moment fromNow>{post?.data()?.timestamp?.toDate()}</Moment>
                         </span>
                     </div>
 
@@ -82,10 +85,10 @@ const Post = ({post}) => {
                 </div>
 
                 {/* Post text */}
-                <p className="text-gray-800 text-[15px] sm:text-[16px] mb-2">{post.data().text}</p>
+                <p className="text-gray-800 text-[15px] sm:text-[16px] mb-2">{post?.data()?.text}</p>
 
                 {/* Post image */}
-                <img className="rounded-2xl mr-2" src={post.data().image}/>
+                <img className="rounded-2xl mr-2" src={post?.data()?.image}/>
 
                 {/* icons */}
                 <div className="flex justify-between text-gray-500 p-2">
@@ -95,7 +98,7 @@ const Post = ({post}) => {
                                 if (!session) {
                                     signIn()
                                 } else {
-                                    setPostId(post?.id)
+                                    setPostId(id)
                                     setOpen(!open)
                                 }
                             }} 
@@ -105,7 +108,7 @@ const Post = ({post}) => {
                             <span className="text-sm">{comments.length}</span>
                         )}
                     </div>
-                    {session?.user.uid === post?.data().id && (
+                    {session?.user.uid === post?.data()?.id && (
                         <TrashIcon onClick={deletePost} className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100"/>
                     )}
                     <div className="flex items-center">
